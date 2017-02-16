d28c0130dae82b90e29e3bf49d0ca0e0959b8ed7
Kvineeth123 <vineethsaikareti@gmail.com>
2017-02-16 23:11:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,7 +39,7 @@ b1.onclick = function() {
                var names=request.responseText;
                names=JSON.parse(names);
                var list='';
-               for(var i=0;i<names.lenght;i++)
+               for(var i=0;i<names.lenghth;i++)
                {
                    list+='<li>'+ names[i] + '<li>';
                }
