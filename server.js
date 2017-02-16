d1aa7c03a57428b74cc6037bf31c0c7dec7f8585
Kvineeth123 <vineethsaikareti@gmail.com>
2017-02-16 15:31:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,56 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+	'article-one':{
+	    title:`article-one-vineeth sai kareti`,
+	    heading:'this is my heading',
+	    content:`
+	    <p> this is my paragraph</p>
+	    <p> this is my paragraph</p>
+	    <p> this is my paragraph</p>
+	    <p> this is my paragraph</p>
+	    <p> this is my paragraph</p>
+	    <p> this is my paragraph</p>
+	    <p> this is my paragraph</p>
+	    <p> this is my paragraph</p>
+	    <p> this is my paragraph</p>
+	    `
+	}
+};
+function createtemplate(data){
+	var title=data.title;
+	var heading=data.heading;
+	var content=data.content;
+	var htmltemplate= `
+	<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+    </head>
+    <body>
+        <div>
+            <a href='\'>HOME</a>
+        </div>
+        <hr/>
+        <h3>${heading}</h3>
+        <div>
+                ${content}
+        </div>
+    </body>
+</html>
+`;
+return htmltemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:article',function(req,res){
+  var an = req.parameter.article;
+  res.send(createtemplate(articles[an]));
 });
 
 app.get('/ui/style.css', function (req, res) {
