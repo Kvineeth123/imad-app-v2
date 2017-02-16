fae042852b9081818a8aeb450e9b7b67fe5e9df5
Kvineeth123 <vineethsaikareti@gmail.com>
2017-02-16 21:40:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,12 +27,13 @@ counter.onclick=function(){
    request.open('GET','http://kvineeth123.imad.hasura-app.io/counter');
    request.send(null);
 };
-    var nameval = document.getElementById('name');
-    var n1=nameval.value;
+
 //submitting name
 var b1 = document.getElementById('btn');
 b1.onclick = function() {
-    list='<li>it worked<li>';
+    var nameval = document.getElementById('name');
+    var n1=nameval.value;
+    list='<li>'+n1+'<li>';
     var ul = document.getElementById('nl');
     ul.innerHTML=list;
 };
