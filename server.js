03352ca5295468784056ba3be5a3cb20fdcb5517
Kvineeth123 <vineethsaikareti@gmail.com>
2017-03-11 22:58:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user : 'kvineeth123',
+    database: 'kvineeth123',
+    host: 'db.hasura.imad-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
