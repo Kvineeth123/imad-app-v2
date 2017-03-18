d3cca88796e741a01367ddc6cc75de00625af0f5
Kvineeth123 <vineethsaikareti@gmail.com>
2017-03-18 11:04:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/', function (req, res) {
 });
 
 function hash(input,salt){
-    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,14,'sha512');
     return ['pbkdf2',salt,10000,hashed.toString('hex')].join('$');
 }
 app.get('/hash/:input',function(req,res){
