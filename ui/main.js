861365cbbad3f7b8cd1a3473a4ced6cad6af62ea
Kvineeth123 <vineethsaikareti@gmail.com>
2017-03-23 02:23:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -70,6 +70,7 @@ b1.onclick = function() {
            }
            
        }
+       loadLogin();
    };
    var username=document.getElementById('username').value;
    var password=document.getElementById('password').value;
