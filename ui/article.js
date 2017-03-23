d4359d9b742370d6d72a1b98e4afa648e0530c6c
Kvineeth123 <vineethsaikareti@gmail.com>
2017-03-23 17:54:34 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -70,6 +70,7 @@ function loadComments () {
         if (request.readyState === XMLHttpRequest.DONE) {
             var comments = document.getElementById('comments');
             if (request.status === 200) {
+                alert("trying to load comments");
                 var content = '';
                 var commentsData = JSON.parse(this.responseText);
                 for (var i=0; i< commentsData.length; i++) {
