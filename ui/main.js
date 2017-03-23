857ed649a16e9bb242e7c01cb54c8d0b93a7d3b2
Kvineeth123 <vineethsaikareti@gmail.com>
2017-03-23 14:54:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -54,57 +54,68 @@ b1.onclick = function() {
    request.send(null);
 };
 */
-// log in function
-var b1 = document.getElementById('login');
-b1.onclick = function() {
-   var request = new XMLHttpRequest();
-   request.onreadystatechange = function(){
-       if(request.readyState===XMLHttpRequest.DONE){
-           if(request.status===200){
-                alert('logged in successfully');     
-           }else if(request.status===403){
-                alert('username/password error');
-               
-           }else if(request.status===400){
-                alert('error in database');
-           }
-           
-       }
-       /*loadLogin();*/
-   };
-   var username=document.getElementById('username').value;
-   var password=document.getElementById('password').value;
-   console.log(username);
-   console.log(password);
-   request.open('POST','http://kvineeth123.imad.hasura-app.io/login',true);
-   request.setRequestHeader('Content-Type','application/json');
-   request.send(JSON.stringify({username:username, password:password}));
-};
-//register function
-var b2 = document.getElementById('reg');
-b2.onclick = function() {
-   var request = new XMLHttpRequest();
-   request.onreadystatechange = function(){
-       if(request.readyState===XMLHttpRequest.DONE){
-           if(request.status===200){
-                alert('user created successfully');     
-           }else if(request.status===403){
-                alert('error in creating user try again');
-               
-           }
-           
-       }
-   };
-   var username=document.getElementById('username').value;
-   var password=document.getElementById('password').value;
-   console.log(username);
-   console.log(password);
-   request.open('POST','http://kvineeth123.imad.hasura-app.io/create-user',true);
-   request.setRequestHeader('Content-Type','application/json');
-   request.send(JSON.stringify({username:username, password:password}));
-};
+//loading login form
+function loadloginform(){
+            var loginArea = document.getElementById('login_area');
+            loginArea.innerHTML = `<center><h3> User not logged in </h3>
+            <form>
+            Username:<input type="text" id="username" size="20"/><br/>
+            Password:<input type="password" id="password" size="20"/><br/>
+            <input type="submit" id="login" value="login"/>
+            <input type="submit" id="reg" value="register"/>
+            </form></center>`;
+            // log in function
+                var b1 = document.getElementById('login');
+                b1.onclick = function() {
+                   var request = new XMLHttpRequest();
+                   request.onreadystatechange = function(){
+                       if(request.readyState===XMLHttpRequest.DONE){
+                           if(request.status===200){
+                                alert('logged in successfully');     
+                           }else if(request.status===403){
+                                alert('username/password error');
+                               
+                           }else if(request.status===400){
+                                alert('error in database');
+                           }
+                           
+                       }
+                       /*loadLogin();*/
+                   };
+                   var username=document.getElementById('username').value;
+                   var password=document.getElementById('password').value;
+                   console.log(username);
+                   console.log(password);
+                   request.open('POST','http://kvineeth123.imad.hasura-app.io/login',true);
+                   request.setRequestHeader('Content-Type','application/json');
+                   request.send(JSON.stringify({username:username, password:password}));
+                };
+                //register function
+                var b2 = document.getElementById('reg');
+                b2.onclick = function() {
+                   var request = new XMLHttpRequest();
+                   request.onreadystatechange = function(){
+                       if(request.readyState===XMLHttpRequest.DONE){
+                           if(request.status===200){
+                                alert('user created successfully');     
+                           }else if(request.status===403){
+                                alert('error in creating user try again');
+                               
+                           }
+                           
+                       }
+                   };
+                   var username=document.getElementById('username').value;
+                   var password=document.getElementById('password').value;
+                   console.log(username);
+                   console.log(password);
+                   request.open('POST','http://kvineeth123.imad.hasura-app.io/create-user',true);
+                   request.setRequestHeader('Content-Type','application/json');
+                   request.send(JSON.stringify({username:username, password:password}));
+                };
+    
+}
 //loading login details
-/*
 function loadLoggedInUser (username) {
     var loginArea = document.getElementById('login_area');
     loginArea.innerHTML = `
@@ -123,8 +134,7 @@ function loadLogin () {
             if (request.status === 200) {
                 loadLoggedInUser(this.responseText);
             } else {
-                var loginArea = document.getElementById('login_area');
-                loginArea.innerHTML = '<center><h3> User not logged in </h3></center>';
+                loadloginform();
             }
         }
     };
@@ -134,5 +144,4 @@ function loadLogin () {
 }
 
 loadLogin();
-*/
 
