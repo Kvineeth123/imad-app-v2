699888ec94b107ea8a0aaa148abb28ed653b8da8
Kvineeth123 <vineethsaikareti@gmail.com>
2017-03-23 19:22:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,6 +71,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+   res.send(counter.toString()); 
+});
+
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
+});
+
 
 function hash (input, salt) {
     // How do we create a hash?
@@ -226,11 +236,6 @@ app.get('/articles/:articleName', function (req, res) {
     }
   });
 });
-
-app.get('/ui/:fileName', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
-});
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
